ad94a35bbf18d9fcfc0bca1d12e5fb56f9c1c8cc
such <suchen9209@126.com>
2021-11-02 23:20:02 +0800

    列表样式

--- src/room/RoomListPanel.jsx
+++ src/room/RoomListPanel.jsx
@@ -10,38 +10,55 @@ function RoomListPanel() {
     //     cursor: "pointer",
     //     borderRadius:14
     // };
-    const baseLine = {
+    const topLine = {
         marginTop : 10,
         fontSize:28,
-        position:"absolute",
     }
-    const baseColumn = {
-        display: "inline-block",
-        marginLeft:0
+    const roomLine = {
+        marginTop : 10,
+        fontSize:20,
+        background:"url('/img/line_bg.png')"
     }
-    let column_1 = new ColumnBase(60);
-    let column_2 = new ColumnBase(40);
-    let column_3 = new ColumnBase(300);
-    let column_4 = new ColumnBase(40);
-    let column_5 = new ColumnBase(40);
+    let column_1 = new ColumnBase(34,86);
+    let column_2 = new ColumnBase(40,300);
+    let column_3 = new ColumnBase(40,100);
+    let column_4 = new ColumnBase(40,120);
+    let column_5 = new ColumnBase(40,120);
 
     return (
         <div className="Room-list-panel">
-            <div style={baseLine}>
+            <div style={topLine}>
                 <span style={column_1}>房间号</span>
                 <span style={column_2}>房间名</span>
                 <span style={column_3}>房主</span>
                 <span style={column_4}>当前人数</span>
                 <span style={column_5}>房间状态</span>
             </div>
+            <div style={roomLine}>
+                <span style={column_1}>1005</span>
+                <span style={column_2}>suchot的房间</span>
+                <span style={column_3}>suchot</span>
+                <span style={column_4}>8</span>
+                <span style={column_5}>DOING</span>
+            </div>
+            <div style={roomLine}>
+                <span style={column_1}>1006</span>
+                <span style={column_2}>wuke的房间</span>
+                <span style={column_3}>wuke</span>
+                <span style={column_4}>6</span>
+                <span style={column_5}>WAITING</span>
+            </div>
         </div>
     )
 }
 
-function ColumnBase(left){
+function ColumnBase(left,width){
     this.display = "inline-block";
     this.marginLeft = left;
     this.marginTop = 10;
+    this.width = width;
+    this.overflow = "hidden";
+
 }
 
 export default RoomListPanel
